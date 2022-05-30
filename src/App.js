c65a99f8d4137a4b5aecc043f981c36190d27866
manu <manuel6benamo@gmail.com>
2022-05-30 21:53:30 +1200

    segundo commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hola que tal
         </a>
       </header>
     </div>
