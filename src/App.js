189e37b8293c4a4ec2946d4d1bac5e7d82297d0c
manu <manuel6benamo@gmail.com>
2022-06-09 20:55:15 +1200

    cuarto commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ItemListContainer } from './components/ItemListContainer/ItemListContai
 function App() {
   return <>
   <Navbar />
-  <ItemListContainer />
+  <ItemListContainer nombreUsuario ="Usuario" />
   
   </>;
 }
