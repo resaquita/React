fcdad4179da76c978ea30bdbf0836f62d8c33dca
ManuB <manuel6benamo@gmail.com>
2022-08-29 07:00:36 +1200

    corregido cart checkout

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,14 +2,22 @@ import "./CartWidget.css"
 
 import React, { useContext, useEffect, useState } from "react"
 import { CartContext } from "../../Context/CartContext"
-import { Link } from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 
 
 
 export const CartWidget = () => {
     const {cart} = useContext(CartContext)
 
+    const {user} = useContext(CartContext)
+
     const [count,setCount] = useState()
+
+    const itemNavigator = () =>{
+        if(!user.user){navigate("/login")}
+        else{navigate("/cart")}
+    }
+    let navigate = useNavigate()
 
     useEffect(()=>{
         const cartCount = cart.reduce((partialSum, item) => partialSum + item.count, 0)
@@ -17,9 +25,7 @@ export const CartWidget = () => {
     },[cart])
     
 return <div>
-    <Link className="divWidget" to="/cart"> 
-        <img className="widget" alt="widget" src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png"></img>
-    </Link>
-    <span className="countWidget">{count}</span>
-</div>
+        <img onClick={()=>itemNavigator()} className="widget" alt="widget" src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png"></img>
+        <span className="countWidget">{count}</span>
+    </div>
 }
